94d39238207c9bf0eaa91bd5c999edb8edb920e0
ligang <ligang1@xsyxsc.com>
2021-03-05 16:32:27 +0800

    feat: initial commit

---
+++ src/components/top.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="top-container">
+    <div class="left-box">
+      <div class="icon-wrapper">
+        <span class="iconfont icon-home"></span>
+        <span class="iconfont icon-sami-select"></span>
+        <span class="iconfont icon-full-screen"></span>
+      </div>
+      <div class="history-wrapper">
+        <span class="iconfont icon-arrow-lift"></span>
+        <span class="iconfont icon-arrow-right"></span>
+      </div>
+    </div>
+    <div class="right-box">
+      <el-input size="small" placeholder="搜索" v-model="query">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data(){
+    return {
+      query:''
+    }
+  }
+};
+</script>
+
+<style  scoped>
+
+</style>
